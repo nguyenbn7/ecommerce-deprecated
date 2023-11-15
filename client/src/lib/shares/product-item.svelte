92e0a55d1322182e889dd9e3f5eede7735c3484e
shadowdeveloper95 <shadowdeveloper95@gmail.com>
2023-11-15 19:18:45 +0700

    product card and shop page

---
+++ client/src/lib/shares/product-item.svelte
@@ -0,0 +1,62 @@
+<div class="card shadow-sm">
+	<div class="image position-relative">
+		<img src="https://placehold.co/600x400/png" alt="Product name here" class="img-fluid bg-info" />
+		<div class="d-flex align-items-center justify-content-center hover-overlay">
+			<button class="btn btn-success bi bi-cart me-2" title="Add to cart"></button>
+			<a class="btn btn-primary" href="/shop/1" title="Detail"><i class="bi bi-info-circle"></i></a>
+		</div>
+	</div>
+
+	<div class="card-body h-100 d-flex flex-column">
+		<a href={'#'} class="text-decoration-none">
+			<h5 class="text-uppercase">Product name here</h5>
+		</a>
+		<span class="mb-2" style="font-size: 1.5em;">$100.00</span>
+	</div>
+</div>
+
+<style lang="scss">
+	.btn {
+		width: 30%;
+		height: 40px;
+	}
+
+	.image :hover {
+		cursor: pointer;
+		opacity: 1;
+
+		& button {
+			transform: none;
+			opacity: 1;
+		}
+
+		& a {
+			transform: none;
+			opacity: 1;
+		}
+	}
+
+	.hover-overlay {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		left: 0;
+		background: rgba(255, 255, 255, 0.5);
+		opacity: 0;
+		transition: all 0.5s;
+
+		& button {
+			z-index: 1000;
+			transition: all 0.5s;
+		}
+
+		& button:first-of-type {
+			transform: translateX(-20px);
+		}
+
+		& a:last-of-type {
+			transform: translateX(20px);
+		}
+	}
+</style>
--- client/src/routes/(shop)/shop/+page.svelte
+++ client/src/routes/(shop)/shop/+page.svelte
@@ -1,1 +1,84 @@
-<h2>Shop</h2>
+<script>
+	import ProductItem from '$lib/shares/product-item.svelte';
+</script>
+
+<div class="container">
+	<div class="row">
+		<div class="col-3">
+			<div class="my-2">
+				<h5 class="text-warning ms-3">Sort By</h5>
+				<select class="form-select mb-4">
+					<option>Alphabetical</option>
+				</select>
+			</div>
+			<div class="my-2">
+				<h5 class="text-warning ms-3">Brands</h5>
+				<ul class="list-group">
+					<li class="list-group-item">Chym Co</li>
+				</ul>
+			</div>
+			<div class="my-2">
+				<h5 class="text-warning ms-3">Types</h5>
+				<ul class="list-group">
+					<li class="list-group-item">Vipe</li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-9">
+			<div class="d-flex justify-content-between align-items-center pb-2">
+				<!-- <app-paging-header [totalItems]="totalItems" [pageNumber]="shopParams.pageNumber"
+                    [pageSize]="shopParams.pageSize"></app-paging-header> -->
+				<div class="d-flex mt-2">
+					<input type="text" placeholder="Search" class="form-control me-2" />
+					<button type="button" class="btn btn-outline-primary mx-2"> Search </button>
+					<button type="button" class="btn btn-outline-danger">Clear</button>
+				</div>
+			</div>
+
+			<div class="row row-cols-3 g-3 mb-4">
+				<div class="col">
+					<ProductItem></ProductItem>
+				</div>
+				<div class="col">
+					<ProductItem></ProductItem>
+				</div>
+				<div class="col">
+					<ProductItem></ProductItem>
+				</div>
+				<div class="col">
+					<ProductItem></ProductItem>
+				</div>
+				<div class="col">
+					<ProductItem></ProductItem>
+				</div>
+				<div class="col">
+					<ProductItem></ProductItem>
+				</div>
+				<div class="col">
+					<ProductItem></ProductItem>
+				</div>
+				<div class="col">
+					<ProductItem></ProductItem>
+				</div>
+				<div class="col">
+					<ProductItem></ProductItem>
+				</div>
+				<div class="col">
+					<ProductItem></ProductItem>
+				</div>
+				<div class="col">
+					<ProductItem></ProductItem>
+				</div>
+			</div>
+
+			<div class="d-flex justify-content-center mb-3">
+				<!-- <app-pager
+					[totalItems]="totalItems"
+					[pageSize]="shopParams.pageSize"
+					(pageChanged)="onPageChanged($event)"
+				></app-pager> -->
+				Pagination here
+			</div>
+		</div>
+	</div>
+</div>
