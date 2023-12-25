70fc39678c9afea15e714a3cdc449da2c44d7897
shadowdeveloper95 <shadowdeveloper95@gmail.com>
2023-12-25 21:53:07 +0700

    add testimonial

--- client/src/routes/(navbar)/(home)/+page.svelte
+++ client/src/routes/(navbar)/(home)/+page.svelte
@@ -2,11 +2,53 @@
 	import { ECOMMERCE_NAME } from '$lib/share/constant';
 	import { onMount } from 'svelte';
 	import { icon } from '@fortawesome/fontawesome-svg-core';
-	import { faCheck, faPhoneVolume, faSync, faTruckFast } from '@fortawesome/free-solid-svg-icons';
+	import {
+		faCheck,
+		faPhoneVolume,
+		faQuoteLeft,
+		faQuoteRight,
+		faSync,
+		faTruckFast
+	} from '@fortawesome/free-solid-svg-icons';
 	import { tns } from 'tiny-slider';
 	import '$lib/layout/(nobars)/about/style.scss';
 
 	const heroSectionId = 'heroSection';
+
+	const features = [
+		{ icon: faCheck, name: 'Quality Product' },
+		{ icon: faTruckFast, name: 'Free Shipping' },
+		{ icon: faSync, name: '7-Day Return' },
+		{ icon: faPhoneVolume, name: '24/7 Support' }
+	];
+
+	const heroImages = ['hero1.jpg', 'hero2.jpg', 'hero3.jpg'];
+
+	const logoImages = ['angular.png', 'netcore.png', 'react.png', 'redis.png', 'typescript.png'];
+
+	const clients = [
+		{
+			name: 'Saira Hakim',
+			profileImage: 'avatar1.png',
+			companyName: 'Local shop owner',
+			quote:
+				'Sed ut perspiciatis unde omnis iste natus error veritatis et quasi architecto beatae vitae dict eaque ipsa quae ab illo inventore Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium'
+		},
+		{
+			name: 'David Niph',
+			profileImage: 'avatar2.png',
+			companyName: 'Local shop owner',
+			quote:
+				'Sed ut perspiciatis unde omnis iste natus error veritatis et quasi architecto beatae vitae dict eaque ipsa quae ab illo inventore Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium'
+		},
+		{
+			name: 'Jacob Sikim',
+			profileImage: 'avatar3.png',
+			companyName: 'Local shop owner',
+			quote:
+				'Sed ut perspiciatis unde omnis iste natus error veritatis et quasi architecto beatae vitae dict eaque ipsa quae ab illo inventore Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium'
+		}
+	];
 
 	onMount(async () => {
 		new (await import('bootstrap')).Carousel(document.getElementById(heroSectionId), {
@@ -16,10 +58,10 @@
 		});
 
 		tns({
+			container: '.logo-slider',
 			items: 1,
-			slideBy: 'page',
 			autoplay: true,
-			speed: 400,
+			speed: 1000,
 			autoplayButtonOutput: false,
 			controls: false,
 			responsive: {
@@ -34,18 +76,16 @@
 				}
 			}
 		});
+
+		tns({
+			container: '.testimonial-slider',
+			items: 1,
+			autoplay: true,
+			speed: 1500,
+			autoplayButtonOutput: false,
+			controls: false
+		});
 	});
-
-	const features = [
-		{ icon: faCheck, name: 'Quality Product' },
-		{ icon: faTruckFast, name: 'Free Shipping' },
-		{ icon: faSync, name: '7-Day Return' },
-		{ icon: faPhoneVolume, name: '24/7 Support' }
-	];
-
-	const heroImages = ['hero1.jpg', 'hero2.jpg', 'hero3.jpg'];
-
-	const logoImages = ['angular.png', 'netcore.png', 'react.png', 'redis.png', 'typescript.png'];
 </script>
 
 <svelte:head>
@@ -68,7 +108,11 @@
 	<div class="carousel-inner">
 		{#each heroImages as image, idx}
 			<div class="carousel-item" class:active={idx === 0}>
-				<img src="/images/{image}" class="m-0 min-vw-100 min-vh-100 hero-img" alt="slide" />
+				<img
+					src="/images/home/hero/{image}"
+					class="m-0 min-vw-100 min-vh-100 hero-img"
+					alt="slide"
+				/>
 			</div>
 		{/each}
 	</div>
@@ -112,21 +156,64 @@
 
 <!-- Featured End -->
 
+<!-- Deals -->
+
+<!-- Discount -->
+
+<!-- New Arrivals -->
+<!-- <div class="container">New Arrivals</div> -->
+
+<!-- We are -->
+
+<!-- Testimonials -->
+<div class="py-5 my-5">
+	<div class="container">
+		<h2 class="display-5 fw-light text-center">
+			What Customers Said
+			<div
+				class="border-bottom mb-5 mt-2 border-4 border-warning mx-auto"
+				style="width: 1.7em;"
+			></div>
+		</h2>
+		<div class="row align-items-center justify-content-center testimonial-slider mt-5">
+			{#each clients as client}
+				<div class="col-xl-3 col-sm-6 text-center">
+					<div class="client-avatar">
+						<img src="/images/home/testimonial/{client.profileImage}" alt={client.name} />
+					</div>
+					<div class="client-meta">
+						<h3>{client.name} <span>{client.companyName}</span></h3>
+						<p class="testimonial-body">
+							"{client.quote}"
+						</p>
+						<span class="quote-icon">
+							{@html icon(faQuoteRight).html}
+						</span>
+					</div>
+				</div>
+			{/each}
+		</div>
+	</div>
+</div>
+<!-- News -->
+
 <!-- Logo Start -->
-<div class="container py-5 overflow-x-hidden">
-	<h2 class="display-5 fw-light text-center">
-		Popular Brands
-		<div
-			class="border-bottom mb-5 mt-2 border-4 border-warning mx-auto"
-			style="width: 1.7em;"
-		></div>
-	</h2>
-	<div class="row align-items-center justify-content-center slider">
-		{#each logoImages as image}
-			<div class="single-logo-item col-xl-3 col-sm-6 text-center position-relative">
-				<img src="/images/home/company-logos/{image}" alt="" />
-			</div>
-		{/each}
+<div class="py-5 my-5">
+	<div class="container overflow-x-hidden">
+		<h2 class="display-5 fw-light text-center">
+			Top Brands
+			<div
+				class="border-bottom mb-5 mt-2 border-4 border-warning mx-auto"
+				style="width: 1.7em;"
+			></div>
+		</h2>
+		<div class="row align-items-center justify-content-center logo-slider mt-5">
+			{#each logoImages as image}
+				<div class="single-logo-item col-xl-3 col-sm-6 text-center position-relative">
+					<img src="/images/home/company-logo/{image}" alt="" />
+				</div>
+			{/each}
+		</div>
 	</div>
 </div>
 
@@ -153,5 +240,48 @@
 
 	.single-logo-item:hover {
 		opacity: 0.7;
+	}
+
+	/**
+	* Clients
+	*/
+	.client-avatar {
+		margin-bottom: 20px;
+	}
+
+	.client-meta h3 {
+		font-size: 20px;
+		font-weight: 600;
+	}
+
+	.client-meta h3 span {
+		display: block;
+		font-size: 70%;
+		margin-top: 10px;
+		color: #051922;
+		font-weight: 600;
+		opacity: 0.5;
+	}
+
+	p.testimonial-body {
+		font-size: 17px;
+		font-style: italic;
+		width: 700px;
+		margin: 0 auto;
+		line-height: 1.8;
+		color: #999999;
+		margin-top: 20px;
+	}
+
+	.quote-icon {
+		margin-top: 20px;
+		font-size: 25px;
+		opacity: 0.3;
+	}
+
+	.client-avatar img {
+		max-width: 100px;
+		border-radius: 50%;
+		margin: 0 auto;
 	}
 </style>
