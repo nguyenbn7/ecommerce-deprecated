9a17531f6bd8f14f5c3c829a89304d0bd6cf9ee4
shadowdeveloper95 <shadowdeveloper95@gmail.com>
2023-12-26 15:39:17 +0700

    discount section

--- client/src/lib/layout/(navbar)/(home)/discount-section.svelte
+++ client/src/lib/layout/(navbar)/(home)/discount-section.svelte
@@ -1,7 +1,40 @@
-<section class="shop-banner">
+<section class="shop-banner" style="background-image: url(/images/home/discount/1.jpg);">
 	<div class="container">
-		<h3>December sale is on! <br /> with big <span class="orange-text">Discount...</span></h3>
+		<h3>December sale is on! <br /> with big <span class="text-warning">Discount...</span></h3>
 		<div class="sale-percent"><span>Sale! <br /> Upto</span>50% <span>off</span></div>
-		<a href="/shop" class="cart-btn btn-lg">Shop Now</a>
+		<a href="/shop" class="btn btn-warning">Shop Now</a>
 	</div>
 </section>
+
+<style lang="scss">
+	.shop-banner {
+		position: relative;
+		background-size: cover;
+		padding: 110px 0px 115px;
+	}
+
+	.shop-banner h3 {
+		position: relative;
+		font-size: 50px;
+		line-height: 1.2em;
+		margin-bottom: 0px;
+	}
+
+	.shop-banner .sale-percent {
+		position: relative;
+		font-size: 60px;
+		font-weight: 700;
+		color: #f28123;
+	}
+
+	.shop-banner .sale-percent span {
+		position: relative;
+		font-size: 24px;
+		line-height: 1.1em;
+		color: #051922;
+		font-weight: 400;
+		text-align: center;
+		margin-right: 10px;
+		display: inline-block;
+	}
+</style>
